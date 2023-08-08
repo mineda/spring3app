2ddf91f3604dd9d8cc47fbdefdb8a168c8e33ded
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-08-08 00:08:14 +0000

    Gitpod config and database

---
+++ .gitpod.Dockerfile
@@ -0,0 +1,3 @@
+FROM gitpod/workspace-mysql
+
+RUN sdk install java 17.0.8-oracle < /dev/null;
---
+++ DDL.sql
@@ -0,0 +1,52 @@
+drop schema if exists spring;
+
+drop user if exists 'user'@'localhost';
+
+create schema spring;
+
+create user 'user'@'localhost' identified by 'pass123';
+
+grant select, insert, delete, update on spring.* to user@'localhost';
+
+use spring;
+
+create table usr_usuario (
+  usr_id bigint unsigned not null auto_increment,
+  usr_nome varchar(20) not null,
+  usr_senha varchar(100) not null,
+  primary key (usr_id),
+  unique key uni_usuario_nome (usr_nome)
+);
+
+create table aut_autorizacao (
+  aut_id bigint unsigned not null auto_increment,
+  aut_nome varchar(20) not null,
+  primary key (aut_id),
+  unique key uni_aut_nome (aut_nome)
+);
+
+create table uau_usuario_autorizacao (
+  usr_id bigint unsigned not null,
+  aut_id bigint unsigned not null,
+  primary key (usr_id, aut_id),
+  foreign key aut_usuario_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade,
+  foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
+);
+
+create table ant_anotacao (
+  ant_id bigint unsigned not null auto_increment,
+  ant_texto varchar(256) not null,
+  ant_data_hora datetime not null,
+  ant_usr_id bigint unsigned not null,
+  primary key (ant_id),
+  foreign key ant_usr_fk (ant_usr_id) references usr_usuario(usr_id)
+);
+
+insert into usr_usuario (usr_nome, usr_senha)
+  values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
+insert into aut_autorizacao (aut_nome)
+  values ('ROLE_ADMIN');
+insert into uau_usuario_autorizacao (usr_id, aut_id) 
+  values (1, 1);
+insert into ant_anotacao(ant_texto, ant_data_hora, ant_usr_id)
+  values('Meu novo projeto', '2023-08-01 19:10', 1);
